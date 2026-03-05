5a67d749728335049f3c60e0641e4a29361d65fa
Gor <166315638+Qwiiwl@users.noreply.github.com>
2026-03-05 14:23:15 +0500

    Update CardConfirmRequest.java

--- src/main/java/uzumtech/paymentservice/dto/request/CardConfirmRequest.java
+++ src/main/java/uzumtech/paymentservice/dto/request/CardConfirmRequest.java
@@ -2,9 +2,11 @@ package uzumtech.paymentservice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record CardConfirmRequest(
         @NotNull Long userId,
-        @NotBlank String cardNumber,
-        @NotNull String otpCode
+        @NotBlank @Pattern(regexp = "\\d{16}", message = "Card number must be exactly 16 digits")
+        String cardNumber,
+        @NotBlank String otpCode
 ) {}

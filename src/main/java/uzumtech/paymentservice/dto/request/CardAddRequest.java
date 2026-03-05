6c90c1fcebc284caab573fe9c66e647d29bd8a9f
Gor <166315638+Qwiiwl@users.noreply.github.com>
2026-03-05 14:22:56 +0500

    Add validation for cardNumber in CardAddRequest

--- src/main/java/uzumtech/paymentservice/dto/request/CardAddRequest.java
+++ src/main/java/uzumtech/paymentservice/dto/request/CardAddRequest.java
@@ -2,8 +2,10 @@ package uzumtech.paymentservice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record CardAddRequest(
         @NotNull Long userId,
-        @NotBlank String cardNumber
+        @NotBlank @Pattern(regexp = "\\d{16}", message = "Card number must contain exactly 16 digits")
+        String cardNumber
 ) {}
